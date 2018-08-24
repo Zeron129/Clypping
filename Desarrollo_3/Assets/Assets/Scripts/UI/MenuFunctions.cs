7f5201a57fa92d50e54c7dc330af7ce35b2adab0
Jeremiah <jeremiah.ag97@gmail.com>
2018-08-23 22:34:55 -0300

    Prototipo de Menu V.1.1
    Minima funcionalidad del Menu, inicia otra escena, accede a opciones y quita el juego

--- Desarrollo_3/Assets/Assets/Scripts/UI/MenuFunctions.cs
+++ Desarrollo_3/Assets/Assets/Scripts/UI/MenuFunctions.cs
@@ -1,16 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuFunctions : MonoBehaviour {
+    public void Playgame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void QuitGame() {
+        Application.Quit();
+    }
 }
