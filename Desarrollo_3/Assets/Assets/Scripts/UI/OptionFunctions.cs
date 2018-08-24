dbc62eeb9b6b50d376ddbb914f5bf5389d2a1ca2
Jeremiah <jeremiah.ag97@gmail.com>
2018-08-24 00:05:19 -0300

    Prototipo de Menu V.1.2
    Desarrollo de  menú de Opciones, desde el cual se puede controlar el volumen maestro, cambiar la calidad de los gráficos y setear el juego en Modo Ventana o Fullscreen.

---
+++ Desarrollo_3/Assets/Assets/Scripts/UI/OptionFunctions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionFunctions : MonoBehaviour {
+    [SerializeField] AudioMixer Master;
+    public void SetVolume(float volume) {
+        Master.SetFloat("MasterVolume", volume);
+    }
+
+    public void setQuality(int QualityIndex) {
+        QualitySettings.SetQualityLevel(QualityIndex);
+    }
+
+    public void FullscreenChange(bool fullscreen) {
+        Screen.fullScreen=fullscreen;
+    }
+}
