a4da48cdc582c2148a8d7210f01de3a1ac5dec9e
Jeremiah <jeremiah.ag97@gmail.com>
2018-08-26 20:19:28 -0300

    Funcionalidad de Script(SpawnLetter)
    SpawnLetter crea palabras mediante strings dados en el inspector

--- Desarrollo_3/Assets/Assets/Scripts/GameMec/SpawnLetter.cs
+++ Desarrollo_3/Assets/Assets/Scripts/GameMec/SpawnLetter.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class SpawnLetter : MonoBehaviour {
     [SerializeField] GameObject prefab;
+    [SerializeField] GameObject Palabra;
     [SerializeField] Sprite[] Sprites;
+    [SerializeField] string MyStrg;
+    char[] StrgToChar;
 
     public void MakeSprite() {
-        int ArrIndx = Random.Range(0,Sprites.Length);
-        Sprite Lettersprite = Sprites[ArrIndx];
+        string UpperSTR = MyStrg.ToUpper();
+        
+        StrgToChar = UpperSTR.ToCharArray();
+        GameObject String = Instantiate(Palabra);
+        String.name = MyStrg;
 
-        GameObject Character = Instantiate(prefab);
+        for (int i = 0; i < StrgToChar.Length; i++) {
+            if (MakeLetter(StrgToChar[i],i,String.transform))
+            {
+                Debug.Log(StrgToChar[i]);
 
-        Character.name = Lettersprite.name;
-        Character.GetComponent<SpriteRenderer>().sprite = Lettersprite;
+            }
+            else
+            {
+                Debug.Log("No se encontro la letra");
+            }
+        }
+    }
+
+    public bool MakeLetter(char Letter,int pos,Transform Padre) {
+        for (int i = 0; i < Sprites.Length; i++)
+        {
+            if (Sprites[i].name == Letter.ToString()) {
+                Vector3 posicion=new Vector3();
+                posicion.x = Sprites[i].bounds.size.x*pos;
+                GameObject Character = Instantiate(prefab,Padre);
+                Character.transform.position = posicion;
+                Character.name = Sprites[i].name;
+                Character.GetComponent<SpriteRenderer>().sprite = Sprites[i];
+                return true;
+            }
+            
+        }
+        return false;
     }
 }
