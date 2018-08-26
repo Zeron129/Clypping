472750dd6820c3052b1813804410941042dbdaa6
Jeremiah <jeremiah.ag97@gmail.com>
2018-08-25 22:19:56 -0300

    Generar Sprites en pantalla(Testing)
    Generacion  de sprites random en base de un spritesheet

---
+++ Desarrollo_3/Assets/Assets/Scripts/GameMec/SpawnLetter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnLetter : MonoBehaviour {
+    [SerializeField] GameObject prefab;
+    [SerializeField] Sprite[] Sprites;
+
+    public void MakeSprite() {
+        int ArrIndx = Random.Range(0,Sprites.Length);
+        Sprite Lettersprite = Sprites[ArrIndx];
+
+        GameObject Character = Instantiate(prefab);
+
+        Character.name = Lettersprite.name;
+        Character.GetComponent<SpriteRenderer>().sprite = Lettersprite;
+    }
+}
