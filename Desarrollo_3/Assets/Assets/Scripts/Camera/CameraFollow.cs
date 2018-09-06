bc21d89cc789359b58101601595db8a4a44cd765
Jeremiah <jeremiah.ag97@gmail.com>
2018-09-06 15:15:48 -0300

    Agregado de Ragdoll y Script de seguimiento para camara

---
+++ Desarrollo_3/Assets/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    [SerializeField] Transform Target;
+    [SerializeField] float smooth =0.125f;
+    [SerializeField] Vector3 offset;
+    // Update is called once per frame
+    private void LateUpdate() {
+        Vector3 desiredPos = Target.position + offset;
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smooth);
+        transform.position = smoothedPos;
+    }
+}
