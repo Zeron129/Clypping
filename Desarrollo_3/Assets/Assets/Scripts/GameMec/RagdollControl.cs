d039f69af45892966a78d1437fc750d5a356d277
Jeremiah <jeremiah.ag97@gmail.com>
2018-09-07 01:16:52 -0300

    Ragdoll Controll
    -Avance de script, las manos del ragdoll se pegan a las "Piedras" 
    -Nuevo Ragdoll mas prolijo

--- Desarrollo_3/Assets/Assets/Scripts/GameMec/RagdollControl.cs
+++ Desarrollo_3/Assets/Assets/Scripts/GameMec/RagdollControl.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class RagdollControl : MonoBehaviour {
     [SerializeField] Transform LeftHand;
     [SerializeField] Transform RightHand;
+    [SerializeField] float smoothfloat;
 
     private void OnTriggerStay2D(Collider2D collision) {
-        Debug.Log(collision.name);
+        
         if(collision.tag == "stones") {
             Debug.Log(collision.name);
+            Vector3 lerpedpos = Vector3.Lerp(LeftHand.position, collision.transform.position, smoothfloat);
+            LeftHand.position =lerpedpos;
+            
         }
     }
 }
