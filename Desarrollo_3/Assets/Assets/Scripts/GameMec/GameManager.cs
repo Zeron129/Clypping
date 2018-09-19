b57905c46148437a3a12d582c5160610cd7ace5a
Unknown <kami.zeron@gmail.com>
2018-09-19 10:03:03 -0300

    incorporación de barra de energía
    incorporación de barra de energía que se drena con el tiempo, el juego se reinicia si se acaba

--- Desarrollo_3/Assets/Assets/Scripts/GameMec/GameManager.cs
+++ Desarrollo_3/Assets/Assets/Scripts/GameMec/GameManager.cs
@@ -1,15 +1,25 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
     [SerializeField] int _puntajePorLetra;
     [SerializeField] int _puntajePorPalabra;
+    [SerializeField] int _puntajeParaGanar;
+    [SerializeField] float _maxEnergy;
+    [SerializeField] float _energyDrainVelocity;
+    [SerializeField] string _nombreEscenaVictoria;
+    [SerializeField] string _nombreEscenaDerrota;
     int _puntaje = 0;
+    float _energy = 0;
+    [SerializeField] Image _currentEnergyBar;
+    [SerializeField] Text _ratioText;
 
-    
-    
+
     private GameObject gameObject;
 
     private static GameManager _instance;
@@ -24,9 +34,12 @@ public class GameManager : MonoBehaviour {
     }
     private void Awake() {
         _puntaje = 0;
+        _energy = _maxEnergy;
     }
     private void Update() {
-        VerificarGameOver();
+        StateMachine();
+       // Playng();
+        //Debug.Log("Energy: " + _energy);
     }
     public void AumentarPuntaje () {
         _puntaje += _puntajePorPalabra;
@@ -37,7 +50,34 @@ public class GameManager : MonoBehaviour {
     }
 
     //verifica la condicion de victoria/derrota
-    void VerificarGameOver() {
+    int StateMachine() {
+        if (_puntaje >= _puntajeParaGanar){
+            Win();
+            return 2;
+        }
+         if (_energy <= 0){
+            Lose();
+            return 0;
+         }
+         Playng();
+         return 1;
+    }
+
+    private void Win(){
+       // SceneManager.LoadScene(_nombreEscenaVictoria);
+    }
+
+    private void Lose(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void Playng(){
+        float ratio = _energy / _maxEnergy;
+        _currentEnergyBar.rectTransform.localScale = new Vector3(1, ratio, 1);
+        _ratioText.text = (ratio * 100).ToString("0") + '%';
+
+        _energy -= 1f * Time.deltaTime * _energyDrainVelocity;
 
     }
+
 }
