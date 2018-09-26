3f67170b8eec88cde02de26138322453f1e002ff
Jeremiah <jeremiah.ag97@gmail.com>
2018-09-26 18:56:26 -0300

    Update del script RandomWordGen

--- Desarrollo_3/Assets/Scripts/GameMec/RandomWordGen.cs
+++ Desarrollo_3/Assets/Scripts/GameMec/RandomWordGen.cs
@@ -3,17 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RandomWordGen : MonoBehaviour {
-    private List<string> words;
-    private static string[] wordList = {"freckle","fresh","charge","steep","avant","cruel" };
+    private static List<string> words = new List<string>() {      "freckle",
+            "fresh",
+            "charge",
+            "steep",
+            "avant",
+            "cruel",
+            "work"
+    ,"reputation"
+    ,"valid"
+    ,"dive"
+    ,"branch"
+    ,"tax"
+    ,"notebook"
+    ,"compact"
+    ,"flatware"
+    ,"cotton"
+    ,"rock"
+    ,"combination"
+    ,"stuff"
+    ,"hero"
+    ,"pray"
+    ,"primary"
+    ,"monkey"
+    ,"publication"
+    ,"repeat"
+    ,"climate"
+    ,"computer"
+    ,"drama"
+    ,"literacy"
+    ,"film"
+    ,"fund"
+    ,"notorious"
+    ,"architect"
+    ,"ignorant"
+    ,"trouser"}
+      ;
 
-    private void Start()
-    {
-
+    private static void Start()
+    {/*
+        for (int i = 0; i < wordList.Length; i++)
+        {
+            words.Add(wordList[i]);
+        }*/
     }
 
     public static string GetRandomWord () {
-        int randomIndex = Random.Range(0, wordList.Length);
-        string randomWord = wordList[randomIndex];
+        Debug.Log("Hay " + words.Count + " palabras");
+        int randomIndex = Random.Range(0, words.Count);
+        string randomWord = words[randomIndex];
+        words.Remove(randomWord);
         return randomWord;
     }
 }
