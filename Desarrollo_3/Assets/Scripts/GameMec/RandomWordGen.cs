a666a4f35732f2c1890a3e73cd3b2289bb983b21
Jeremiah <jeremiah.ag97@gmail.com>
2018-09-26 02:21:59 -0300

    Optimizacion de la Lista de Palabras

--- Desarrollo_3/Assets/Scripts/GameMec/RandomWordGen.cs
+++ Desarrollo_3/Assets/Scripts/GameMec/RandomWordGen.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RandomWordGen : MonoBehaviour {
+    private List<string> words;
     private static string[] wordList = {"freckle","fresh","charge","steep","avant","cruel" };
+
+    private void Start()
+    {
+
+    }
 
     public static string GetRandomWord () {
         int randomIndex = Random.Range(0, wordList.Length);
