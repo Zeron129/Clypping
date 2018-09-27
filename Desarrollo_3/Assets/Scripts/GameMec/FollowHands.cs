8580a056773be8f2b058055fe17158d816995a9d
Unknown <unknown@example.com>
2018-09-27 17:04:07 -0300

    Script Nuevo "Follow Hands"
    El personaje sigue correctamente a sus manos mientras haciende.
    -Le hace falta un movimiento mas suave

---
+++ Desarrollo_3/Assets/Scripts/GameMec/FollowHands.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowHands : MonoBehaviour {
+    [SerializeField] Transform Hand1;
+    [SerializeField] Transform Hand2;
+    [SerializeField] float smooth = 0.125f;
+    [SerializeField] Vector3 offset;
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        Vector3 smoothedPos = Vector3.Lerp(Hand1.localPosition, Hand2.localPosition, 0.5f);
+        transform.localPosition = smoothedPos + offset;
+    }
+}
